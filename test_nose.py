6cdc57e3fd36feef80865ccb72c4091d343593e7
Alexander Couzens <lynxis@fe80.eu>
2015-11-16 05:03:45 +0100

    add test_nose for testing with pynose
    using pynose it's much easier to create tests.
    test_nose expect to have 2 environment variables set.
    CLIENT_REV and SERVER_REV. To have multi-matrix tests, do
    this via jenkins

---
+++ test_nose.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+import test_td
+
+# random hash
+CONTEXT = None
+
+# lxc container
+SERVER = None
+CLIENT = None
+
+# pids of tunneldigger client and server
+SERVER_PID = None
+CLIENT_PID = None
+
+def setup_module():
+    CONTEXT = test_td.get_random_context()
+    CLIENT, SERVER = test_td.prepare_containers(CONTEXT, os.environ['CLIENT_REV'], os.environ['SERVER_REV'])
+    SERVER_PID = test_td.run_server(SERVER)
+    CLIENT_PID = test_td.run_client(CLIENT)
+
+def teardown_module():
+    for cont in [CLIENT, SERVER]:
+        if cont and cont.running:
+            cont.shutdown(5)
+
+class TestTunneldigger(object):
+    def test_ping_tunneldigger_server(self):
+        # ping 192.168.254.1
+        pass
+
+    def test_wget_tunneldigger_server(self):
+        # wget http://192.168.254.1
+        pass
+
+    def test_ensure_tunnel_up_for_5m(self):
+        # get id of l2tp0 iface
+        ## ip -o l | awk -F: '{ print $1 }'
+        # sleep 5 minutes
+        # get id of l2tp0 iface
+        ## ip -o l | awk -F: '{ print $1 }'
+        # assert early_id == later_id
+        pass
