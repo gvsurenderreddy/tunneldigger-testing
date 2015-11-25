7d2cfd0e8d28683cb657a141a2ebe0ba12cbf7bc
Alexander Couzens <lynxis@fe80.eu>
2015-11-25 13:04:21 +0100

    jenkins: update OLD_REV

--- jenkins.sh
+++ jenkins.sh
@@ -29,7 +29,7 @@ export CLIENT_REV=$NEW_REV
 export SERVER_REV=$NEW_REV
 nosetests3
 
-OLD_REV="c638231efca6b3a6e1c675ac0834a3e851ad1bdc 7cbe5d60c1b72415e15e573e0a9189f47a3f2094"
+OLD_REV="c638231efca6b3a6e1c675ac0834a3e851ad1bdc 263eb59098fd11990b6ab75933fb7633055cbc9a 4e4f13cdc630c46909d47441093a5bdaffa0d67f"
 # do client NEW_REV against old revs
 for rev in $OLD_REV ; do
   export CLIENT_REV=$NEW_REV
